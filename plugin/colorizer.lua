8edefb786e1f0811d1e92357bf3f63cdc62bf0f3
Marlon-Sbardelatti <hetzwga@gmail.com>
2026-04-29 10:45:56 -0300

    hipatterns

--- plugin/colorizer.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({"https://github.com/NvChad/nvim-colorizer.lua"})
-require("colorizer").setup()
-
--- return {
---   "NvChad/nvim-colorizer.lua",
---   config = function()
---     require("colorizer").setup()
---   end
--- }
